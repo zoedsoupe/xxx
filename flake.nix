50f18508816f3eeef12036377448ccf3a807cddd
Zoey de Souza Pessanha <zoey.spessanha@outlook.com>
2022-06-13 14:00:15 -0300

    iniital commit :tada:

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = inputs@{ self, utils, rust-overlay, ... }:
+    utils.lib.mkFlake rec {
+      inherit self inputs;
+
+      supportedSystems = [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      sharedOverlays = [ (import rust-overlay) ];
+
+      outputsBuilder = channels: with channels; {
+        packages = with nixpkgs; { 
+          inherit (nixpkgs) package-from-overlays;
+
+          xxx = rustPlatform.buildRustPackage {
+            pname = "xxx";
+            version = "v0.1.0";
+            doCheck = true;
+            src = ./.;
+            checkInputs = [ rustfmt clippy ];
+            checkPhase = ''
+              runHook preCheck
+
+              cargo check
+              rustfmt --check src/**/*.rs
+              cargo clippy
+
+              runHook postCheck
+            '';
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+          };
+        };
+
+        devShell = nixpkgs.mkShell {
+          name = "xxx";
+
+          buildInputs = with nixpkgs; [
+            rust-bin.stable.latest.minimal
+          ];
+        };
+      };
+    };
+}
